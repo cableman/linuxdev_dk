f3f80a49c8ebd97e77d1fafeb846a473aae51aca
Jesper Kristensen <cableman@linuxdev.dk>
2013-11-15 14:38:43 +0100

    Removed the user login changes

--- themes/sepia/template.php
+++ themes/sepia/template.php
@@ -1,22 +1,6 @@
 <?php
 
 function sepia_preprocess_page(&$vars) {
-  //  Removes the tabs from user  login, register & password
-  switch (current_path()) {
-    case 'user':
-      $vars['title'] = t('Login');
-      unset($vars['tabs']);
-      break;
-    case 'user/register':
-      $vars['title'] = t('New account');
-      unset($vars['tabs']);
-      break;
-    case 'user/password':
-      $vars['title'] = t('DOH! I forgot my password');
-      unset($vars['tabs']);
-      break;
-  }
-
   if (isset($vars['node'])) {
     // If the node type is "blog" the template suggestion will be "page--blog.tpl.php".
     $vars['theme_hook_suggestions'][] = 'page__' . $vars['node']->type;
@@ -36,18 +20,4 @@ function sepia_breadcrumb($variables) {
     $output .= '<div class="breadcrumb">' . implode(' » ', $breadcrumb) . '</div>';
     return $output;
   }
-}
-
-function sepia_form_user_login_alter(&$form, &$form_state, $form_id) {
-  $form['name']['#attributes']['placeholder'] = $form['name']['#title'];
-  unset($form['name']['#title']);
-  unset($form['name']['#description']);
-
-  $form['pass']['#attributes']['placeholder'] = $form['pass']['#title'];
-  unset($form['pass']['#title']);
-  unset($form['pass']['#description']);
-
-  $form['openid_identifier']['#attributes']['placeholder'] = $form['openid_identifier']['#title'];
-  unset($form['openid_identifier']['#title']);
-  unset($form['openid_identifier']['#description']);
 }
