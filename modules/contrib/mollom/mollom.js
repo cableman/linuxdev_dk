8ce867b650a96c0eb1134dcf8a25a89ac704e6fe
Jesper Kristensen <cableman@linuxdev.dk>
2014-04-14 23:09:12 +0200

    Updated contrib modules

--- modules/contrib/mollom/mollom.js
+++ modules/contrib/mollom/mollom.js
@@ -45,7 +45,7 @@ function getMollomCaptcha() {
 
   // Retrieve a new CAPTCHA.
   $.ajax({
-    url: Drupal.settings.basePath + path,
+    url: Drupal.settings.basePath + Drupal.settings.pathPrefix + path,
     type: 'POST',
     dataType: 'json',
     success: function (data) {
@@ -59,7 +59,15 @@ function getMollomCaptcha() {
       // Add an onclick-event handler for the new link.
       Drupal.attachBehaviors(context);
       // Focus on the CAPTCHA input.
-      $('input[name="mollom[captcha]"]', context).focus();
+      if (newCaptchaType == 'image') {
+          $('input[name="mollom[captcha]"]', context).focus();
+      } else {
+         // Focus on audio player.
+         // Fallback player code is responsible for setting focus upon embed.
+         if ($('#mollom_captcha_audio').is(":visible")) {
+             $('#mollom_captcha_audio').focus();
+         }
+      }
     }
   });
   return false;
