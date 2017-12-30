d4c8d542568d09e5f660fc2051b6d136440d6b00
Jesper Kristensen <cableman@linuxdev.dk>
2017-12-30 12:15:04 +0100

    Updated module and core

--- sites/all/modules/contrib/fences/templates/field--fences-ul.tpl.php
+++ sites/all/modules/contrib/fences/templates/field--fences-ul.tpl.php
@@ -6,7 +6,6 @@
  * @see http://developers.whatwg.org/grouping-content.html#the-ul-element
  */
 ?>
-
 <?php if ($element['#label_display'] == 'inline'): ?>
   <span class="field-label"<?php print $title_attributes; ?>>
     <?php print $label; ?>:
@@ -24,4 +23,5 @@
       <?php print render($item); ?>
     </li>
   <?php endforeach; ?>
+
 </ul>
